be37c7246dcb5ab9bb2df443860cfd7dc8553d49
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-09-12 01:52:36 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -8,7 +8,7 @@ class Computer < Player
     if !board.taken?(5)
       move = "5"
     
-    elsif board.turn_count > 1
+    elsif board.turn_count == 2
       move = [1, 3, 7, 9].detect{|i| !board.taken?(i)}.to_s
     
     
