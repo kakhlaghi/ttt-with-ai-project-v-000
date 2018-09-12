364357c18688fce778d6f32ab3fb6af22d0aa7d6
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-09-12 01:55:33 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -10,15 +10,13 @@ class Computer < Player
     
     elsif board.turn_count == 2
       move = [1, 3, 7, 9].detect{|i| !board.taken?(i)}.to_s
+  
     
-  else
-    move = rand
-    
-    #elsif board.cells.each.with_index {|cell,index| 
-     #     if !board.taken?(index+1) 
-      #      move = "#{index+1}"
-       #   end }
-      #  move
+    elsif board.cells.each.with_index {|cell,index| 
+          if !board.taken?(index+1) 
+            move = "#{index+1}"
+          end }
+        move
     end
 
 end
